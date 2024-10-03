c4d75c598a3e93bdd18a4928933b5441258cb596
quantum <liberiamor@gmail.com>
2024-10-03 20:15:03 +0300

    exception handler

--- manager-app/src/main/java/org/quantum/managerapp/controller/ProductController.java
+++ manager-app/src/main/java/org/quantum/managerapp/controller/ProductController.java
@@ -1,15 +1,20 @@
 package org.quantum.managerapp.controller;
 
 import org.quantum.managerapp.dto.UpdateProductDto;
+import org.quantum.managerapp.exception.ProductNotFoundException;
 import org.quantum.managerapp.model.Product;
 import org.quantum.managerapp.service.ProductService;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 
 @Controller
@@ -44,5 +49,12 @@ public class ProductController {
     public String deleteProduct(@ModelAttribute Product product) {
 	productService.delete(product.getId());
 	return "redirect:/catalogue/products/list";
+    }
+
+    @ExceptionHandler(ProductNotFoundException.class)
+    public String handleProductNotFoundException(Throwable exception, Model model, HttpServletResponse response) {
+	model.addAttribute("error", exception);
+	response.setStatus(HttpStatus.NOT_FOUND.value());
+	return "errors/not_found";
     }
 }
